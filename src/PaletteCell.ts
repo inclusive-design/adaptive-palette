6be1737a866895df074e8478994ba0fb36a08ff8
Joseph Scheuhammer <clown@alum.mit.edu>
2023-10-19 15:29:57 -0400

    feat: remove extra style and classes from `PaletteCell`'s `props` argument
    Removed the optional `class` and `styles` from the `PaletteCellProps` type.

--- src/PaletteCell.ts
+++ src/PaletteCell.ts
@@ -17,7 +17,6 @@ function debugProps(x) {
   console.log("DEBUGPROPS(): %O", x);
 }
 
-
 type PaletteCellProps = {
   id: string,
   options: {
@@ -27,9 +26,7 @@ type PaletteCellProps = {
     rowStart: number,
     rowSpan: number,
     bciAvId: BciAvId
-  },
-  class?: string,
-  style?: string
+  }
 }
 
 export function PaletteCell (props: PaletteCellProps) {
@@ -38,29 +35,13 @@ export function PaletteCell (props: PaletteCellProps) {
   const { columnStart, columnSpan, rowStart, rowSpan } = props.options;
   const { bciAvId, label } = props.options;
 
-  // Basic styles are the `paletteCell` class defined in PaletteCell.css.
-  // Concatenate any additional classes provided by `props`.
-  let classes = "paletteCell";
-  if (props.class) {
-    classes = `${classes} ${props.class}`;
-  }
-  let disabled = false;
-  if (classes.indexOf("disabled") >= 0) {
-    disabled = true;
-  }
-
-  // Also concatenate local styles with given grid cell styles
-  let styles = `
+  const gridStyles = `
     grid-column: ${columnStart} / span ${columnSpan};
     grid-row: ${rowStart} / span ${rowSpan};
   `;
-  if (props.style) {
-    styles = `${styles} ${props.style}`;
-  }
 
-  // Get svg graphic for the cell
   return html`
-    <button id="${props.id}" class="${classes}" style="${styles}" disabled=${disabled}>
+    <button id="${props.id}" class="paletteCell" style="${gridStyles}" >
       <${BlissSymbol} bciAvId=${bciAvId} label=${label} />
     </button>
   `;
