d75020b87929f3e4f85fe984077fc3b86000c1ea
Joseph Scheuhammer <clown@alum.mit.edu>
2023-11-29 14:20:30 -0500

    feat: move main script to `index.js` and include it within `index.html`

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2023 Inclusive Design Research Centre, OCAD University
+ * All rights reserved.
+ *
+ * Licensed under the New BSD license. You may not use this file except in
+ * compliance with this License.
+ *
+ * You may obtain a copy of the License at
+ * https://github.com/inclusive-design/adaptive-palette/blob/main/LICENSE
+ */
+import { render } from "preact";
+import { html } from "htm/preact";
+import { initAdaptivePaletteGlobals } from "./GlobalData.ts";
+
+// Initialize any globals used elsewhere in the code.
+await initAdaptivePaletteGlobals();
+
+import { Palette } from "./Palette";
+import bmwJson from "./keyboards/bmw_palette.json";
+render (html`<${Palette} json=${bmwJson}/>`, document.getElementById("bmwKeyCodes"));
