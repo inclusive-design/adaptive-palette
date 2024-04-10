765ee6894d48a9040cbc81a30e168f5340e0ac87
Joseph Scheuhammer <clown@alum.mit.edu>
2024-04-10 11:47:20 -0400

    feat: use a signal to update the contents of the encoding area
    Using a signal replaces the code based on preact global states
    handling.

--- src/GlobalData.ts
+++ src/GlobalData.ts
@@ -16,6 +16,7 @@
 import { html } from "htm/preact";
 import { createContext, VNode } from "preact";
 import { useContext, useState } from "preact/hooks";
+import { signal } from "@preact/signals";
 import { EncodingType } from "./index.d";
 
 /**
@@ -79,32 +80,8 @@ export async function initAdaptivePaletteGlobals (mainPaletteContainerId?:string
 }
 
 /**
- * Palette shared states
+ * Signal for updating the contents of the ContentBmwEncoding area.  The value
+ * of the signal is the current array of EncodingType objects to display in the
+ * ContentBmwEncoding area.
  */
-// Create a context to pass the palette states to palette children components
-type PaletteStateType = {
-  fullEncoding: EncodingType[],
-  setFullEncoding: (param: object[]) => void
-};
-
-const defaultPaletteStateContext = {
-  fullEncoding: [],
-  setFullEncoding: () => {}
-};
-const paletteStateContext = createContext<PaletteStateType>(defaultPaletteStateContext);
-
-// Create a provider component that will wrap the components needing access to the global states
-export function paletteStateProvider(props: {children}): VNode {
-  const [fullEncoding, setFullEncoding] = useState([]);
-
-  return html`
-    <${paletteStateContext.Provider} value=${{ fullEncoding, setFullEncoding }}>
-      ${props.children}
-    </${paletteStateContext.Provider}>
-  `;
-}
-
-// Create a custom hook to easily access the global states within components
-export function usePaletteState(): PaletteStateType {
-  return useContext(paletteStateContext);
-}
+export const changeEncodingContents = signal([]);
