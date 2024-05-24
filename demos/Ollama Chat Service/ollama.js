a68c222716f1e9d223bd4ecc8df7897ca8db1861
Joseph Scheuhammer <clown@alum.mit.edu>
2024-05-24 18:02:23 -0400

    feat: retrieve available models from the service and allow users to select one

--- demos/Ollama Chat Service/ollama.js
+++ demos/Ollama Chat Service/ollama.js
@@ -11,6 +11,49 @@
 
 import ollama from "ollama/browser";
 
+// Default name of model used (aka, none).  Set in setSelectedModel() handler.
+let nameOfModelToUse = "";
+
+// Function to retrieve a list of LLM's available from the service
+async function getModelNames () {
+  let modelNames = [];
+  try {
+    const list = await ollama.list();
+    list.models.forEach( (model) => {
+      modelNames.push(model.name);
+    });
+  }
+  catch (error) {
+    console.debug(error);
+  }
+  return modelNames;
+}
+
+// Initialize the model <select> element's options.
+async function initModelSelect () {
+  const modelNames = await getModelNames();
+  const selectElement = document.getElementById("modelSelect");
+  if (modelNames.length === 0) {
+    selectElement.add(new Option("No Available Models"));
+  }
+  else {
+    modelNames.forEach( (aName) => {
+      selectElement.add(new Option(aName));
+    });
+  }
+  // The `nameOfModelToUse` is the empty string even after the <select> is
+  // initialized.  Set `nameOfModelToUse` to the shown <option>, which is the
+  // first one.  Note that it could be "No Avaialable Models".  In that case,
+  // all chat queries will fail with an error meesage.
+  nameOfModelToUse = selectElement.item(0).label;
+}
+
+// Handle model select element when a new selection is made.
+function setSelectedModel () {
+  const selectElement = document.getElementById("modelSelect");
+  nameOfModelToUse = selectElement.selectedOptions[0].label;
+}
+
 // Handle click on "Ask" buttons
 function askClicked(event) {
   const singleSentence = "Answer with a single grammatically correct sentence.";
@@ -28,7 +71,7 @@ function askClicked(event) {
 async function queryChat (query) {
   const message = { role: "user", content: query };
   const response = await ollama.chat({
-    model: "phi3",
+    model: nameOfModelToUse,
     messages: [message],
     stream: true
   });
@@ -58,4 +101,8 @@ async function outputResult(response, outputEl) {
 
 document.getElementById("justAsk").addEventListener("click", askClicked);
 document.getElementById("singleSentence").addEventListener("click", askClicked);
+document.getElementById("modelSelect").addEventListener("change", setSelectedModel);
+
+// Set up the model <select> element
+initModelSelect();
 
