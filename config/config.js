1a2c982af790554105bfbb06451b99f0594d0f7b
Cindy Qi Li <cli@ocadu.ca>
2024-07-11 15:58:46 -0400

    feat: more changes to add the Express server

---
+++ config/config.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2024, Inclusive Design Institute
+ *
+ * Licensed under the BSD 3-Clause License. You may not use this file except
+ * in compliance with this License.
+ *
+ * You may obtain a copy of the BSD 3-Clause License at
+ * https://github.com/inclusive-design/baby-bliss-bot/blob/main/LICENSE
+ */
+
+/* global process */
+
+export const config = {
+  server: {
+    port: process.env.PORT || 3000
+  }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import preact from "@preact/preset-vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  server: {
-    port: 3000
-  },
   css: {
     devSourcemap: true
   },
