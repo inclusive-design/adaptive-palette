e1acfb3445f2b859897e7e6272315ddd6ea32a69
Joseph Scheuhammer <clown@alum.mit.edu>
2025-01-24 11:10:02 -0500

    fix: remove `console.log()` debug statement

--- src/client/GlobalUtils.ts
+++ src/client/GlobalUtils.ts
@@ -58,7 +58,6 @@ function speak(text): void {
  */
 async function loadPaletteFromJsonFile (jsonFilePath: string): Promise<JsonPaletteType> {
   try {
-    console.log(`loadPaletteFromJsonFile(): file path is ${jsonFilePath}`);
     const response = await fetch(jsonFilePath);
     if (!response.ok) {
       console.error(`Error loading ${jsonFilePath}: ${response.status}`);
