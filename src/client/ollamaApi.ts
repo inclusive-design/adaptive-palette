1e6ac7f80ffbc703cb6c9270e44fcbaf6f4df4a8
Joseph Scheuhammer <clown@alum.mit.edu>
2025-04-15 17:40:52 -0400

    feat: add a way to follow the typing restrictions of ollama.ts when testing

--- src/client/ollamaApi.ts
+++ src/client/ollamaApi.ts
@@ -13,7 +13,7 @@ import { default as ollama, ChatResponse } from "ollama/browser";
 
 /**
  * Retrieve a list of LLMs available from the service
- * @return {Array}    - Array of the names of the available models.
+ * @return {Array} - Array of the names of the available models.
  */
 export async function getModelNames (): Promise<string[]> {
   const modelNames = [];
@@ -40,7 +40,7 @@ export async function getModelNames (): Promise<string[]> {
  * @param {String} systemPrompt - Optional system prompt, defaults to the
  *                                empty string.
  * @param Promise<ChatResponse | any>  - The response from the service. Note:
- *                                the value type <amy> is technically
+ *                                the value type <any> is technically
  *                                <<AbortableAsyncIterator<ChatResponse>>,
  *                                when the response is streamed; otherwise
  *                                just <ChatResponse>.  However, the ollama
@@ -59,12 +59,22 @@ export async function queryChat (query: string, modelName: string, streamResp: b
     });
   }
   messageArray.push({ role: "user", content: query });
-  // Something very odd is going on with the overloading of `ollama.chat()`.  If
-  // the call is set up as follows, there is an error when running jest/node.
-  // It is important to note that the error occurs even when no call to
-  // `ollama.chat()` is made.  This implies that some transpiler checker is
-  // reporting the error, not any actually execution of the code.
-  // The error is:
+
+  // `ollama.chat()` is overloaded to support streaming vs. non-streaming and
+  // disallows the following way of calling it when running tests using
+  // jest/node:
+  // ...
+  //   const response = await ollama.chat({
+  //    model: modelName,
+  //    messages: messageArray,
+  //    stream: streamResp,
+  //    keep_alive: 15
+  //   });
+  // ...
+  // The error does not occur when using `vite` to build the distribution.
+  // Also, the error occurs when no call to `ollama.chat()` is made.  This
+  // implies that a transpiler checker in the testing framework is reporting
+  // the error, not any actual execution of the code.  The error is:
   // src/client/ollamaApi.ts:79:5 - error TS2769: No overload matches this call.
   //    Overload 1 of 2, '(request: ChatRequest & { stream: true; }): Promise<AbortableAsyncIterator<ChatResponse>>', gave the following error.
   //      Type 'boolean' is not assignable to type 'true'.
@@ -73,33 +83,19 @@ export async function queryChat (query: string, modelName: string, streamResp: b
   //
   // 79     stream: streamResp,
   //        ~~~~~~
-  //
+  // Related: https://github.com/ollama/ollama-js/issues/78
   // What does work, instead, is an if/else construct to set the `stream`
   // property explicitly to `true` or `false`.
   //
-  console.debug(`streamResp: ${streamResp}, ${typeof streamResp}`);
-  //   const response = await ollama.chat({
-  //    model: modelName,
-  //    messages: messageArray,
-  //    stream: streamResp,
-  //    keep_alive: 15
-  //   });
-  let response;
+  const request = {
+    model: modelName,
+    messages: messageArray,
+    keep_alive: 15
+  };
   if (streamResp) {
-    response = await ollama.chat({
-      model: modelName,
-      messages: messageArray,
-      stream: true,
-      keep_alive: 15
-    });
+    return await ollama.chat({ ...request, stream: true });
   }
   else {
-    response = await ollama.chat({
-      model: modelName,
-      messages: messageArray,
-      stream: false,
-      keep_alive: 15
-    });
+    return await ollama.chat({ ...request, stream: false });
   }
-  return response;
 }
