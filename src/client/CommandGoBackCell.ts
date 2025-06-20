e886254796c076527675b1f24170071f73528ed0
Joseph Scheuhammer <clown@alum.mit.edu>
2025-06-20 16:22:14 -0400

    fix: improve name of optional default container id parameter for `goBackImpl()`

--- src/client/CommandGoBackCell.ts
+++ src/client/CommandGoBackCell.ts
@@ -28,33 +28,36 @@ type CommandGoBackCellPropsType = {
  * The implementation of the function invoked by, for example, activating a
  * CommandGoBackCell button/cell.  This determines which palette to go back to
  * by consulting the navigation stack, adjusts the stack accordingly, and
- * calls upon the palette-to-go-back-to to render itself.
- * @param paletteContainerId {string} - Optional id of the HTMLELement in which
- *                                      to render the palette. Note that if
- *                                      the navigation stack entry declares the
- *                                      container, it is used instead of this
- *                                      parameter.
+ * calls upon the palette-to-go-back-to to render itself.  The
+ * `defaultContaineId` parameter is optional fallback and is only used if the
+ * navigation stack entry does not specify an HTMLElement in which to render
+ * the palette.  If not given, and the navigation stack also does not specify
+ * a rendering container, then the container defaults to the document's
+ * `body` element.
+ * @param defaultContaineId {string} - Optional id of the HTMLELement in which
+ *                                     to render the palette if none is
+ *                                     specified in the navigation stack entry.
  */
-export async function goBackImpl (paletteContainerId?: string ): Promise<void> {
+export async function goBackImpl (defaultContaineId?: string ): Promise<void> {
   const { paletteStore, navigationStack } = adaptivePaletteGlobals;
 
   const paletteToGoBackTo = navigationStack.peek();
   if (paletteToGoBackTo && paletteToGoBackTo.palette) {
     const paletteDefinition = await paletteStore.getNamedPalette(paletteToGoBackTo.palette.name, loadPaletteFromJsonFile);
     if (paletteDefinition) {
-      const paletteContainer = paletteToGoBackTo.htmlElement || document.getElementById(paletteContainerId) || document.body;
+      const paletteContainer = paletteToGoBackTo.htmlElement || document.getElementById(defaultContaineId) || document.body;
       navigationStack.popAndSetCurrent(paletteToGoBackTo);
       render (html`<${Palette} json=${paletteDefinition}/>`, paletteContainer);
     }
     else {
-      console.error(`goBackImpl():  Unable to locate the palette definition for ${paletteToGoBackTo.palette.name}`);
+      console.error(`goBackImpl(): Unable to locate the palette definition for ${paletteToGoBackTo.palette.name}`);
     }
   }
 };
 
 /*
- * Event handler for an CommandGoBackCellPropsType button/cell that, when clicked,
- * goes back one palette.
+ * Event handler for an CommandGoBackCellPropsType button/cell that, when
+ * clicked, goes back one palette.
  */
 const goBackToPalette = async (event: Event): Promise<void> => {
   const button = event.currentTarget as HTMLElement;
